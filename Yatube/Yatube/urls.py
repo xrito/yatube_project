082206cb8141a3234b602dee8c1578454626ec0a
Дмитрий <xrito@yandex.ru>
2021-08-17 16:32:57 +0400

    Добавлены urls и view-функции

--- Yatube/Yatube/urls.py
+++ Yatube/Yatube/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('posts.urls')),
+    path('', include('posts.urls', namespace='group')),
     path('admin/', admin.site.urls),
 ]
--- Yatube/posts/views.py
+++ Yatube/posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Главная страница
+
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+# Страница с постами, отфилтрованные по группам
+
+
+def group_posts(request, slug):
+    return HttpResponse(f'Список групп {slug}')
