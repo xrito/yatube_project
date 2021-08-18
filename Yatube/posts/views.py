c2efb0f003470cdac61167222d792c81dcc0bc6a
Дмитрий <xrito@yandex.ru>
2021-08-18 14:29:46 +0400

    Разбиваем шаблоны: include, block, extends

--- Yatube/posts/views.py
+++ Yatube/posts/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Главная страница
-
-
 def index(request):
-    return HttpResponse('Главная страница')
+    return render(request, 'posts/index.html')
+
+def group_list(request):
+    return render(request, 'posts/group_list.html')    
 
 # Страница с постами, отфилтрованные по группам
-
-
 def group_posts(request, slug):
     return HttpResponse(f'Список групп {slug}')
