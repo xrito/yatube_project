548dc444fad9cf40b65b28d04fb0a512a23c0e11
Дмитрий <xrito@yandex.ru>
2021-08-18 15:28:08 +0400

    Словарь контекста, ветвления, циклы

--- Yatube/posts/views.py
+++ Yatube/posts/views.py
@@ -2,13 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-# Главная страница
+
 def index(request):
-    return render(request, 'posts/index.html')
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+    }
+    return render(request,  template, context)
+
 
 def group_list(request):
-    return render(request, 'posts/group_list.html')    
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+    }
+    return render(request,  template, context)
 
-# Страница с постами, отфилтрованные по группам
+
 def group_posts(request, slug):
     return HttpResponse(f'Список групп {slug}')
